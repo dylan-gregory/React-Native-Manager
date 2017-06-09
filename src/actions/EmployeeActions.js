73718feb1935b4294a6eab4193646248a90a4d05
dylan-gregory <jdylangregory@gmail.com>
2017-06-09 09:48:27 -0400

    Building a DataSource object to display a list view of all employees

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -11,7 +12,6 @@ export const employeeUpdate = ({ prop, value }) => {
     payload: { prop, value }
   };
 };
-
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
@@ -22,6 +22,19 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE });
         Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
+
+
+// this will constantly watch the data source for a change for the entire life of our application
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
       });
   };
 };
